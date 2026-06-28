d82916b7054df266fc8363e755b4faa66e7fec59
zrinafril7-png <zrinafril7@gmail.com>
2026-06-28 11:17:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Entertainment For You berhasil dibuat! ❤️");
+const btn = document.getElementById("startBtn");
+const content = document.getElementById("content");
+
+btn.addEventListener("click", function () {
+    content.classList.remove("hidden");
+    content.classList.add("show");
+    btn.style.display = "none";
+});
